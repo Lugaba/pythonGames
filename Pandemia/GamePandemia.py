25d10f930dcb91b7ad1810565e52926b1860a190
Luca Hummel <luca.hummel12@gmail.com>
2021-10-26 21:01:31 -0300

    Mostrar explicacao no acerto

--- Pandemia/GamePandemia.py
+++ Pandemia/GamePandemia.py
@@ -16,6 +16,7 @@ mutado = False
 pontos = 0
 jogoNovo = True
 selecionados = []
+imagemExp = ""
 
 # IMAGENS
 mutadoImg = "Images/mutado.png"
@@ -137,9 +138,9 @@ def carregarJogo():
     global jogoNovo
     global selecionados
     global cardsGame
+    global imagemExp
 
     pontosText = my_font40.render(f"Pontos: {pontos}", True, white)
-
 
     if jogoNovo:
         shuffle(cardsGame)
@@ -155,6 +156,7 @@ def carregarJogo():
                 telaAtual = 0
                 jogoNovo = True
                 pontos = 0
+                imagemExp = ""
                 cardsGame = cardsImg.copy()
             if (34 < mouseX < 98) and (636 < mouseY < 700):
                 mutado = not mutado
@@ -177,6 +179,8 @@ def carregarJogo():
     screen.fill(azul)
     screen.blit(voltarCarregado, (34, 20))
     screen.blit(pontosText, (540 - pontosText.get_width() / 2, 16))
+    if imagemExp != "":
+        screen.blit(pygame.image.load(imagemExp), (175, 484))
 
     for i in range(0, len(cardsGame)):
         if i > 7:
@@ -199,8 +203,15 @@ def carregarJogo():
 
     if len(selecionados) == 2:
         if cardsGame[selecionados[0]] == cardsGame[selecionados[1]]:
+
             pygame.time.delay(1000)
             pontos += 100
+            for i in range(0, len(cardsGame[selecionados[0]])):
+                if i < len(cardsGame[selecionados[0]])-2 and cardsGame[selecionados[0]][i + 1] == ".":
+                    imagemExp += cardsGame[selecionados[0]][i] + "Exp"
+                    continue
+                imagemExp += cardsGame[selecionados[0]][i]
+            print(imagemExp)
             cardsGame[selecionados[0]] = "Images/Correct.png"
             cardsGame[selecionados[1]] = "Images/Correct.png"
             selecionados = []
